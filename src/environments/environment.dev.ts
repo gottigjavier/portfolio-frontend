5444478533b443d18561e39ec97e6a79c042ebd9
gottigjavier <gottigjavier@gmail.com>
2022-07-11 20:38:35 -0300

    css gird

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,3 +1,11 @@
+/* export const environment = {
+    production: false,
+    apiUrl: "http://localhost",
+    apiUrl: "https://portfoliogottig.herokuapp.com",
+    apiPort: ":8080"
+};
+ */
+
 export const environment = {
     production: false,
     apiUrl: "https://portfoliogottig.herokuapp.com",
