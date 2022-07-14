65f463b4b1369ffc4b3eff50c203af5ba06856f6
gottigjavier <gottigjavier@gmail.com>
2022-07-14 01:01:57 -0300

    API link in footer

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,13 +1,14 @@
-export const environment = {
+/* export const environment = {
     production: false,
     apiUrl: "http://localhost",
     //apiUrl: "https://portfoliogottig.herokuapp.com",
     apiPort: ":8080"
 };
+ */
 
-/* 
+
 export const environment = {
     production: false,
     apiUrl: "https://portfoliogottig.herokuapp.com",
     apiPort: ""
-}; */
+};
